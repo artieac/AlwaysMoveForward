e2ba527d01a170bae1de9891c10a77ceaa86d894
Arthur Correa <artieac@gmail.com>
2014-10-07 21:18:26 -0400

    more role stuff

--- AnotherBlog/Common/DomainModel/RoleType.cs
+++ AnotherBlog/Common/DomainModel/RoleType.cs
@@ -12,8 +12,7 @@ namespace AlwaysMoveForward.AnotherBlog.Common.DomainModel
         static RoleType()
         {
             RoleType.Roles = new Dictionary<RoleType.Id, string>();
-            RoleType.Roles.Add(RoleType.Id.Guest, RoleType.Id.Guest.ToString());
-            RoleType.Roles.Add(RoleType.Id.User, RoleType.Id.User.ToString());
+            RoleType.Roles.Add(RoleType.Id.Reader, RoleType.Id.Reader.ToString());
             RoleType.Roles.Add(RoleType.Id.SiteAdministrator, RoleType.Id.SiteAdministrator.ToString());
             RoleType.Roles.Add(RoleType.Id.Administrator, RoleType.Id.Administrator.ToString());
             RoleType.Roles.Add(RoleType.Id.Blogger, RoleType.Id.Blogger.ToString());
@@ -21,17 +20,15 @@ namespace AlwaysMoveForward.AnotherBlog.Common.DomainModel
 
         public enum Id
         {
-            Guest = 0,
-            User = 1,
-            SiteAdministrator = 2,
-            Administrator = 3,
-            Blogger = 5
+            Administrator = 1,
+            Blogger = 2,
+            Reader = 3,
+            SiteAdministrator = 4
         }
 
         public class Names
         {
-            public const string Guest = "Guest";
-            public const string User = "User";
+            public const string Reader = "Reader";
             public const string SiteAdministrator = "SiteAdministrator";
             public const string Administrator = "Administrator";
             public const string Blogger = "Blogger";
