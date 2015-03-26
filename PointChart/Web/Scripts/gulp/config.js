62ece26cf9e9b6546ddec646940d7702c95ebdd8
Arthur Correa <acorrea@vistaprint.com>
2015-03-26 16:33:04 -0400

    more gulp stuff

--- PointChart/Web/Scripts/gulp/config.js
+++ PointChart/Web/Scripts/gulp/config.js
@@ -6,7 +6,7 @@ module.exports = {
         // A separate bundle will be generated for each
         // bundle config in the list below
         bundleConfigs: [{
-            entries: ['./node_modules/less/dist/*.js', './node_modules/react/dist/*.js'],
+            entries: ['./node_modules/react/dist/react.js'],
             dest: dest,
             outputName: 'appBundle.js',
             // list of externally available modules to exclude from the bundle
