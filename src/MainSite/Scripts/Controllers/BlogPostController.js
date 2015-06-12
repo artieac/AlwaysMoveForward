ae96df6ffedd578cb729321ec9df9d6f402b71b1
Arthur Correa <artieac@gmail.com>
2015-06-11 22:00:19 -0400

    get blog post via api call

---
+++ src/MainSite/Scripts/Controllers/BlogPostController.js
@@ -0,0 +1,13 @@
+﻿theApp.controller('BlogPostController', function ($scope, $resource, $http, $sce) {
+    // @Function
+    // Description  : Triggered while displaying expiry date in Customer Details screen.
+    $scope.formatDate = function (date) {
+        var dateOut = new Date(date);
+        return dateOut;
+    };
+
+    $scope.getMostRecent = function (urlRoot) {
+        var getMostRecentBlogPost = $resource(urlRoot + '/api/BlogPost/MostRecent');
+        $scope.mostRecentBlogPost = getMostRecentBlogPost.get();
+    }    
+});
