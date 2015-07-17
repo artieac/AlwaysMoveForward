4b266c598509abd088d861dabc2ae57605c8d2cc
Arthur Correa <artieac@gmail.com>
2015-07-16 21:23:32 -0400

    adding a loading symbotl

--- src/MainSite/Scripts/Controllers/BlogPostController.js
+++ src/MainSite/Scripts/Controllers/BlogPostController.js
@@ -1,4 +1,6 @@
-﻿theApp.controller('BlogPostController', function ($scope, $resource, $http, $sce) {
+﻿theApp.controller('BlogPostController', function ($scope, $resource, $http, $sce, $q) {
+    $scope.waitingForData = false;
+
     // @Function
     // Description  : Triggered while displaying expiry date in Customer Details screen.
     $scope.formatDate = function (date) {
@@ -7,7 +9,12 @@
     };
 
     $scope.getMostRecent = function (urlRoot) {
+        $scope.waitingForData = true;
         var getMostRecentBlogPost = $resource(urlRoot + '/api/BlogPost/MostRecent');
-        $scope.mostRecentBlogPost = getMostRecentBlogPost.get();
+        getMostRecentBlogPost.get()
+            .$promise.then(function(result){
+                $scope.mostRecentBlogPost = result;
+                $scope.waitingForData = false;
+            });
     }    
 });
