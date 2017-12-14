b3ea1dbad3296046ef08b36407c60bb5237896fc
Arthur Correa <acorrea@vistaprint.com>
2017-12-14 18:59:05 -0500

    update to publish by name

--- ContainerPublish.bat
+++ ContainerPublish.bat
@@ -1,6 +1,6 @@
-docker tag %1 artieac/alwaysmoveforward.oauth2.web:OAuth2
+docker tag alwaysmoveforward.oauth2.web artieac/alwaysmoveforward.oauth2.web:latest
 docker push artieac/alwaysmoveforward.oauth2.web
 docker logout
 powershell "Invoke-Expression" "-Command" "(aws ecr get-login)"
-docker tag %1 215561421394.dkr.ecr.us-.dkr.ecr.us-east-2.amazonaws.com/alwaysmoveforward.oauth2.web
+docker tag alwaysmoveforward.oauth2.web 215561421394.dkr.ecr.us-.dkr.ecr.us-east-2.amazonaws.com/alwaysmoveforward.oauth2.web
 docker push 215561421394.dkr.ecr.us-.dkr.ecr.us-east-2.amazonaws.com/alwaysmoveforward.oauth2.web
