4e9e0e632d042c05862ad783ff5923f76fa139a8
Artie <artieac@gmail.com>
2026-01-28 00:07:13 -0500

    more .net 8 upgrades

--- src/Common/AlwaysMoveForward.Common/Utilities/DefaultLogger.cs
+++ src/Common/AlwaysMoveForward.Common/Utilities/DefaultLogger.cs
@@ -1,85 +1,39 @@
 using System;
-using System.IO;
-using System.Reflection;
-using log4net;
-using log4net.Config;
-using log4net.Repository;
+using Microsoft.Extensions.Logging;
 
 namespace AlwaysMoveForward.Common.Utilities
 {
     public class DefaultLogger : LoggerBase
     {
-        private static bool isConfigured = false;
-        private static readonly object configLock = new object();
-        private static ILoggerRepository logRepository;
+        private readonly ILogger _logger;
 
-        static DefaultLogger()
+        public DefaultLogger() : this(null)
         {
-            ConfigureLog4Net();
         }
 
-        private static void ConfigureLog4Net()
+        public DefaultLogger(ILogger logger)
         {
-            if (!isConfigured)
-            {
-                lock (configLock)
-                {
-                    if (!isConfigured)
-                    {
-                        var assembly = Assembly.GetEntryAssembly() ?? Assembly.GetExecutingAssembly();
-                        logRepository = log4net.LogManager.GetRepository(assembly);
-
-                        // Try to find log4net.config file
-                        var configFile = new FileInfo("log4net.config");
-                        if (configFile.Exists)
-                        {
-                            XmlConfigurator.Configure(logRepository, configFile);
-                        }
-                        else
-                        {
-                            // Fall back to basic configuration
-                            BasicConfigurator.Configure(logRepository);
-                        }
-
-                        isConfigured = true;
-                    }
-                }
-            }
-        }
-
-        private ILog logger = null;
-
-        public ILog Logger
-        {
-            get
-            {
-                if (this.logger == null)
-                {
-                    this.logger = log4net.LogManager.GetLogger(this.GetType());
-                }
-
-                return this.logger;
-            }
+            _logger = logger ?? LogManager.CreateDefaultLogger();
         }
 
         protected override void LogDebug(string message)
         {
-            this.Logger.Debug(message);
+            _logger?.LogDebug(message);
         }
 
         protected override void LogError(string message)
         {
-            this.Logger.Error(message);
+            _logger?.LogError(message);
         }
 
         protected override void LogInfo(string message)
         {
-            this.Logger.Info(message);
+            _logger?.LogInformation(message);
         }
 
         protected override void LogWarn(string message)
         {
-            this.Logger.Warn(message);
+            _logger?.LogWarning(message);
         }
     }
 }
