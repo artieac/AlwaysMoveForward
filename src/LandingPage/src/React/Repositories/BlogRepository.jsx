c9ef70cdb4433827a6151154dad445f8b230d645
Artie <artieac@gmail.com>
2026-05-05 10:38:58 -0400

    fix the blog api call

--- src/LandingPage/src/React/Repositories/BlogRepository.jsx
+++ src/LandingPage/src/React/Repositories/BlogRepository.jsx
@@ -2,7 +2,7 @@ import { RestClient } from './RestClient'
 
 export class BlogRepository extends RestClient {
      getMostRecentPosts(numberToGet, responseHandler) {
-        let getUrl = 'http://blog.alwaysmoveforward.com/api/BlogPosts/' + numberToGet;
+        let getUrl = 'https://blog.alwaysmoveforward.com/api/BlogPosts/' + numberToGet;
         this.getRequest(getUrl, responseHandler);
     }
 }
